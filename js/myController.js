4737d2eca211b25d641b9c5bf6653f10fc514dbd
Khayyam Jones <Khayyam.Jones@gmail.com>
2017-01-16 18:54:17 -0700

    makeing a breaking point

--- js/myController.js
+++ js/myController.js
@@ -2,4 +2,23 @@ angular.module("myApp")
     .controller("myController", function($scope) {
 
         $(".search").hide();
+
+        var people = [
+            {
+                name: 'Chris Johnson',
+                tagline: 'some awesome tagline about you',
+                description: 'Lorem Enim anim excepteur reprehenderit fugiat ea magna do aliquip est eu exercitation culpa in. Officia aute aute adipisicing commodo sint nulla nostrud amet. Deserunt mollit enim aute irure adipisicing id veniam cupidatat. Elit ipsum in ullamco sit enim ullamco reprehenderit anim aliqua. Commodo dolore nisi ipsum officia nostrud sunt duis ex ut fugiat et duis magna cillum eiusmod aliqua duis. Enim consectetur sunt in cupidatat deserunt aliqua nostrud non culpa ipsum irure. Cillum nulla excepteur et Lorem enim ipsum labore eiusmod voluptate qui anim aliqua dolore anim velit amet.',
+                sq-pic: './images/profile-pics/blonde-bun.jpg'
+            },
+            {
+                name: 'Sally Rally',
+                tagline: 'Quis et mollit anim mollit eu tempor',
+                description: 'Lorem Irure in laborum dolore duis dolor elit duis elit. Ut ut ad laboris cupidatat deserunt nisi consequat. Eu sit consectetur veniam do non nulla ut esse excepteur duis. Cillum Lorem nisi do incididunt veniam adipisicing reprehenderit commodo officia reprehenderit laborum irure est aliquip eu excepteur mollit. Dolore tempor laborum excepteur laboris dolor dolore aliqua et sunt ea do duis do amet nulla. Minim minim consequat tempor sint veniam consequat et aliqua ad voluptate nulla tempor aliquip eu cupidatat eu qui. Quis esse proident deserunt amet laborum in nulla.',
+                sq-piq: './images/profile-pics/sally.jpg'
+            }
+        ];
+
+        var profileUpdater = [
+
+        ]
     });
